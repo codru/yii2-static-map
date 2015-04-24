83ee25669849dafc03c6faf54037a061604f6e16
vasilevm <maksim311289@mail.ru>
2015-04-24 16:35:32 +0300

    added tests for extension

---
+++ tests/unit/_config.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'id' => 'app-web',
+    'class' => 'yii\web\Application',
+    'language' => 'en',
+    'basePath' => \Yii::getAlias('@tests'),
+    'runtimePath' => \Yii::getAlias('@tests/_output'),
+    'bootstrap' => [],
+];
