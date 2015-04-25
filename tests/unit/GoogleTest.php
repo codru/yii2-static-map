5733ce7f8604c7d41429a0c461786df658a33fe7
vasilevm <maksim311289@mail.ru>
2015-04-25 12:49:33 +0300

    update tests

---
+++ tests/unit/GoogleTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Codru Components
+ *
+ * @author Maxim Vasiliev <codrilla@gmail.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+class GoogleTest extends \yii\codeception\TestCase
+{
+    public $appConfig = '@tests/unit/_config.php';
+
+    public function testGetMapUrl()
+    {
+        $map = [
+            'class' => \codru\staticmap\types\Google::className(),
+            'options' => [
+                'center' => '40,50',
+                'zoom' => '13',
+                'size' => '640x100',
+                'scale' => '2',
+                'language' => Yii::$app->language,
+                'markers' => [
+                    'size' => 'tiny',
+                    '40,50',
+                ],
+            ],
+        ];
+        $this->assertEquals(
+            'http://maps.googleapis.com/maps/api/staticmap?center=40,50&zoom=13&size=640x100&scale=2&language=en&markers=size:tiny|40,5',
+            Yii::createObject($map)->getMapUrl()
+        );
+    }
+}
---
+++ tests/unit/OpenStreetTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Codru Components
+ *
+ * @author Maxim Vasiliev <codrilla@gmail.com>
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+
+class OpenStreetTest extends \yii\codeception\TestCase
+{
+    public $appConfig = '@tests/unit/_config.php';
+
+    public function testGetMapUrl()
+    {
+        $map = [
+            'class' => \codru\staticmap\types\OpenStreet::className(),
+            'options' => [
+                'center' => '40,50',
+                'zoom' => '15',
+                'size' => '1024x200',
+                'language' => Yii::$app->language,
+                'markers' => [
+                    '40,50',
+                    'ol-marker',
+                ],
+            ],
+        ];
+        $this->assertEquals(
+            'http://staticmap.openstreetmap.de/staticmap.php?center=40,50&zoom=15&size=1024x200&language=en&markers=40,50,ol-marke',
+            Yii::createObject($map)->getMapUrl()
+        );
+    }
+}
